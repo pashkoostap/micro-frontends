d82bddec6cfb9550fdab988c0c84070b568aa455
Pashko Ostap <pashko.ostap@gmail.com>
2021-02-26 12:51:40 +0200

    added StylesProvider to container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { MarketingApp } from './apps';
 import { Header } from './components';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'con',
+});
+
 const App = () => (
   <div>
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   </div>
 );
 
