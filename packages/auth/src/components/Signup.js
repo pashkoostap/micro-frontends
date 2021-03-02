22e770afd7257d6b2fa2de77c9674fbf90755014
Pashko Ostap <pashko.ostap@gmail.com>
2021-03-02 16:27:27 +0200

    fixed bug with Signup form

--- packages/auth/src/components/Signup.js
+++ packages/auth/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
